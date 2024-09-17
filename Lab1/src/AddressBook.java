32f386eee0b86e0e27f6f81cef8a6fc9278ab030
Hybried8 <oluwatosinajakaye04@gmail.com>
2024-09-16 20:32:07 -0400

    Hi hi

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    public ArrayList<BuddyInfo> buds;
+
+    public void addBuddy(){
+
+    }
+
+    public void removeBuddy(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
