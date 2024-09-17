18c92089c720f6d7f090f74442415d90fbb16679
Hybried8 <oluwatosinajakaye04@gmail.com>
2024-09-16 20:47:44 -0400

    Changed to add

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -3,8 +3,12 @@ import java.util.ArrayList;
 public class AddressBook {
     public ArrayList<BuddyInfo> buds;
 
-    public void addBuddy(){
+    public AddressBook() {
+        buds = new ArrayList<>();
+    }
 
+    public void addBuddy(BuddyInfo bud){
+        buds.add(bud);
     }
 
     public void removeBuddy(){
@@ -12,6 +16,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        System.out.println(addressBook);
     }
 }
