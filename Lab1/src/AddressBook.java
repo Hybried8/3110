f5d2cb3af6a6877077ab3f6ba7e7a0d87ff295b1
Hybried8 <oluwatosinajakaye04@gmail.com>
2024-09-16 20:48:47 -0400

    Changed to remove

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -11,14 +11,15 @@ public class AddressBook {
         buds.add(bud);
     }
 
-    public void removeBuddy(){
-
+    public void removeBuddy(BuddyInfo bud){
+        buds.remove(bud);
     }
 
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
         System.out.println(addressBook);
     }
 }
